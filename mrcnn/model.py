fb06f7eda142c5c84129bfc557b3e54e8db6f26d
Valentin Rosca <valentin.bg.rosca@gmail.com>
2019-09-10 17:39:13 +0300

    Increase mask size to 112x112

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1003,7 +1003,9 @@ def build_fpn_mask_graph(rois, feature_maps, image_meta,
     x = KL.TimeDistributed(KL.Conv2DTranspose(256, (2, 2), strides=2, activation="relu"),
                            name="mrcnn_mask_deconv")(x)
     x = KL.TimeDistributed(KL.Conv2DTranspose(256, (2, 2), strides=2, activation="relu"),
-                           name="mrcnn_mask_deconv+2")(x)
+                           name="mrcnn_mask_deconv_2")(x)
+    x = KL.TimeDistributed(KL.Conv2DTranspose(256, (2, 2), strides=2, activation="relu"),
+                           name="mrcnn_mask_deconv_3")(x)
     x = KL.TimeDistributed(KL.Conv2D(num_classes, (1, 1), strides=1, activation="sigmoid"),
                            name="mrcnn_mask")(x)
     return x
